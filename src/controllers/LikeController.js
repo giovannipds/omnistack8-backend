86b32a7895b1953046b7d3f3f26b5dedd7ca8014
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-11 17:54:51 -0300

    Learn how to get headers parameteres

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -3,6 +3,7 @@ const Dev = require('../models/Dev');
 module.exports = {
   store(req, res) {
     console.log(req.params.devId);
+    console.log(req.headers.user);
 
     return res.json({ ok: true });
   }
