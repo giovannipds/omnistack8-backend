8813f62b83472d4bad596717f47a5e96bd821633
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-11 17:00:37 -0300

    Connect to a mongodb db with mongoose and mongodb atlas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-otrr0.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
