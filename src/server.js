1e918c20f9fad3a1385fd9cc5de3fc651ede1c76
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-18 11:59:52 -0300

    Require socket io + extract + use different server

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const io = require('socket.io');
 const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const httpServer = express();
+const server = require('http').Server(httpServer);
+
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-otrr0.mongodb.net/omnistack8?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+httpServer.use(cors());
+httpServer.use(express.json());
+httpServer.use(routes);
 
 server.listen(3333);
