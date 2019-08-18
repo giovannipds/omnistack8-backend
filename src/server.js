bfcb8195d4f92f9bf8c1ef07db4be3514d8361ad
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-18 12:00:22 -0300

    Rename httpServer to app

--- src/server.js
+++ src/server.js
@@ -5,16 +5,16 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const httpServer = express();
-const server = require('http').Server(httpServer);
+const app = express();
+const server = require('http').Server(app);
 
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-otrr0.mongodb.net/omnistack8?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
-httpServer.use(cors());
-httpServer.use(express.json());
-httpServer.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
