8b552dae2aba794211cba590fee6a30cc43db95f
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-18 12:02:30 -0300

    Log when someone connect to our socket

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const io = require('socket.io');
+
 const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
+io.on('connection', socket => {
+  console.log('Nova conexão', socket.id);
+});
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-otrr0.mongodb.net/omnistack8?retryWrites=true&w=majority', {
   useNewUrlParser: true
